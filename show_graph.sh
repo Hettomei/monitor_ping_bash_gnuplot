7ef5e2837174da3d777a5eb3d139704a232f3095
Timothée Gauthier <timothee.gauthier@consertotech.pro>
2018-08-20 20:19:19 +0100

    Better regex

--- show_graph.sh
+++ show_graph.sh
@@ -5,7 +5,9 @@ FILEPATH="data/$FILE"
 ls "$FILEPATH"
 DATAPATH="data/$FILE.dat"
 
-grep "bytes from" "$FILEPATH" | sed -E 's/\[([0-9]+\.[0-9]+)\].*time=([0-9.]+) ms/\1 \2/' > "$DATAPATH"
+# grep to remove first line of PING
+# sed to extract timestamp and duration
+grep "bytes from" "$FILEPATH" | sed -E 's/\[([0-9.]+)\].*time=([0-9.]+) ms/\1 \2/' > "$DATAPATH"
 
 gnuplot -p -e "set title 'Ping duration in milliseconds;
 lower is better';
